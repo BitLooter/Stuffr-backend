a7c65f289c3963a04d324f2d679d4d2a43924b81
BitLooter <pwnzerdragoon+gh@gmail.com>
2016-12-31 03:14:11 -0800

    Improved default settings

--- debugconfig-example.py
+++ debugconfig-example.py
@@ -1,3 +1,9 @@
 """Settings for debugging."""
 
+from sqlalchemy.engine.url import URL
+
+DEBUG = True
 SECRET_KEY = 'DEBUG KEY DO NOT USE IN PRODUCTION'
+
+# Default database information
+SQLALCHEMY_DATABASE_URI = str(URL(drivername='sqlite', database='stuffr.db'))
